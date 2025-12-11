f3878998b32634641c07c66e015a76203ff22852
Xlebo <samo.chlebo@gmail.com>
2025-12-11 23:11:11 +0100

    [day09] Future bird, why are you impossible to hold?

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -3,10 +3,14 @@ package day09
 import getOrFetchInputData
 import getTestInput
 import kotlin.math.abs
-import kotlin.math.pow
-import kotlin.math.sqrt
 
 fun main() {
+
+    fun exclusiveRangeOf(first: Long, second: Long): LongRange = if (first > second) {
+        second + 1..<first
+    } else {
+        first + 1..<second
+    }
 
     fun part1(input: List<String>): Long =
         input.map { row -> row.split(",").map { it.toLong() } }.map { it[0] to it[1] }.let { points ->
@@ -15,21 +19,23 @@ fun main() {
                     first to points[secondIndex]
                 }
             }
-        }
-            .maxOf { (abs(it.first.first - it.second.first) + 1) * (abs(it.first.second - it.second.second) + 1) }
+        }.maxOf { (abs(it.first.first - it.second.first) + 1) * (abs(it.first.second - it.second.second) + 1) }
 
 
-//    fun part2(input: List<String>): Long {
-//    }
+    fun part2(input: List<String>): Long {
+        val horizontalLines = mutableMapOf<Long, LongRange>()
+        val verticalLines = mutableMapOf<Long, LongRange>()
+        return 0L
+    }
 
     val testInput = getTestInput(9, "example")
     val test1 = part1(testInput)
     check(test1 == 50L) { "Got $test1" }
 
-//    val test2 = part2(testInput)
-//    check(test2 == 25272L) { "Got $test2" }
+    val test2 = part2(testInput)
+    check(test2 == 24L) { "Got $test2" }
 
     val input = getOrFetchInputData(9)
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
---
+++ src/day10/Day10.kt
@@ -0,0 +1,21 @@
+package day10
+
+fun main() {
+
+//    fun part1(input: List<String>, cables: Int): Int {
+//    }
+
+//    fun part2(input: List<String>): Long {
+//    }
+
+//    val testInput = getTestInput(8, "example")
+//    val test1 = part1(testInput, 10)
+//    check(test1 == 40) { "Got $test1" }
+//
+//    val test2 = part2(testInput)
+//    check(test2 == 25272L) { "Got $test2" }
+//
+//    val input = getOrFetchInputData(8)
+//    println(part1(input, 1000))
+//    println(part2(input))
+}
